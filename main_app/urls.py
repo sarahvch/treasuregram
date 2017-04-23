07325fbe70bedede4d1dec33ee67b84c15dc64df
sarahvch <sarahvch@gmail.com>
2017-04-23 12:12:17 -0600

    added detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^([0-9]+)/$', views.detail, name = 'detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -13,7 +13,9 @@ def index(request):
     treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures':treasures})
 
-
+def detail(request, treasure_id):
+    treasure = Treasure.objects.get(id=treasure_id)
+    return render(request, 'detail.html', {'treasure':treasure})
 # class Treasure:
 #     def __init__(self,name,value,material,location,img_url):
 #         self.name = name
