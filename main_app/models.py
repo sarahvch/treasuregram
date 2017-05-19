6d4b87502d22bdd91c48f68626b6cd3a965577e8
sarahvch <sarahvch@gmail.com>
2017-05-19 11:19:16 -0600

    added like buttons

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,8 @@ class Treasure(models.Model):
     material = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     image = models.ImageField(upload_to='treasure_images',default='media/default.png')
-
+    likes = models.IntegerField(default=0)
+    
     def __str__(self):
         return self.name
 # Create your models here.
---
+++ main_app/static/js/main.js
@@ -0,0 +1,12 @@
+$('button').on('click', function(event){
+  event.preventDefault();
+  var element = $(this);
+  $.ajax({
+    url : '/like_treasure/',
+    type : 'GET',
+    data : { treasure_id : element.attr("data-id")},
+    success : function(response){
+      element.html(' ' + response);
+    }
+  });
+});
